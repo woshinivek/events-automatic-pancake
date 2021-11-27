05cfee0b2842469d775ce9a6f35c299b6acac456
woshinivek <woshinivek@yahoo.com>
2021-11-27 19:07:53 +0300

    changes

--- js/02-form-submit-event.js
+++ js/02-form-submit-event.js
@@ -1,3 +1,38 @@
+// const formEl = document.querySelector(".js-register-form");
+// const butnEl = document.querySelector(".magic-button");
+
+// formEl.addEventListener("submit", onFormSubmit);
+// // butnEl.addEventListener("mouseup", onMouseUpChangeColor);
+// // butnEl.removeEventListener("mouseup", onMouseUpChangeColor);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   console.dir(event.currentTarget);
+//   //   console.log(event.currentTarget.elements.email.value);
+//   //   console.log(event.currentTarget.password.value);
+//   const formElements = event.currentTarget.elements;
+//   console.log(formElements);
+
+//   const mail = formElements.email.value;
+//   const password = formElements.password.value;
+//   const subscription = formElements.subscription.value;
+
+//   const values = {
+//     mail,
+//     password,
+//     subscription,
+//   };
+
+//   console.log(values);
+// }
+
+// function onMouseUpChangeColor(event) {
+//   butnEl.classList.add("magic-btn-clicked");
+// }
+
+// -------------------------------------------------------------------
+
 const formEl = document.querySelector(".js-register-form");
 const butnEl = document.querySelector(".magic-button");
 
@@ -8,11 +43,18 @@ formEl.addEventListener("submit", onFormSubmit);
 function onFormSubmit(event) {
   event.preventDefault();
 
-  console.dir(event.currentTarget);
-  console.log(event.currentTarget.elements.email.value);
-  console.log(event.currentTarget.password.value);
-}
+  const formData = new FormData(event.currentTarget);
 
-// function onMouseUpChangeColor(event) {
-//   butnEl.classList.add("magic-btn-clicked");
-// }
+  console.dir(formData);
+
+  //   const entries = formData.entries();
+
+  //   for (const entrie of entries) {
+  //     console.log(entrie);
+  //   }
+
+  formData.forEach((value, name) => {
+    console.log("name:", name);
+    console.log("value:", value);
+  });
+}
