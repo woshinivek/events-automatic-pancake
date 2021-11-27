0ae3b00c0066b2b239374d03efe3d093429c4cf5
woshinivek <woshinivek@yahoo.com>
2021-11-27 17:25:14 +0300

    changes

--- js/02-form-submit-event.js
+++ js/02-form-submit-event.js
@@ -2,9 +2,17 @@ const formEl = document.querySelector(".js-register-form");
 const butnEl = document.querySelector(".magic-button");
 
 formEl.addEventListener("submit", onFormSubmit);
+// butnEl.addEventListener("mouseup", onMouseUpChangeColor);
+// butnEl.removeEventListener("mouseup", onMouseUpChangeColor);
 
 function onFormSubmit(event) {
   event.preventDefault();
 
   console.dir(event.currentTarget);
+  console.log(event.currentTarget.elements.email.value);
+  console.log(event.currentTarget.password.value);
 }
+
+// function onMouseUpChangeColor(event) {
+//   butnEl.classList.add("magic-btn-clicked");
+// }
