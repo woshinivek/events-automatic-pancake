68a906419dc0c07b16e755d131d4cce709ebbd13
woshinivek <woshinivek@yahoo.com>
2021-12-01 19:40:47 +0300

    changes

--- js/04-keyboard-events.js
+++ js/04-keyboard-events.js
@@ -2,3 +2,9 @@ const refs = {
   btnClear: document.querySelector(".js-clear"),
   output: document.querySelector(".js-output"),
 };
+
+window.addEventListener("keypress", onOutputChange);
+
+function onOutputChange(event) {
+  console.log("code: ", event.code);
+}
