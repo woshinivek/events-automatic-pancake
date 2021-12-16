cf9a0e20c89ed091f5ac2bcd1b83c71a7de1f20a
woshinivek <woshinivek@yahoo.com>
2021-12-16 21:00:51 +0300

    changes

--- js/06-modal.js
+++ js/06-modal.js
@@ -1,6 +1,42 @@
+// const refs = {
+//   openBtn: document.querySelector('[data-action="open-modal"]'),
+//   closeBtn: document.querySelector('[data-action="close-modal"]'),
+//   backdrop: document.querySelector(".js-backdrop"),
+// };
+
+// refs.openBtn.addEventListener("click", onModalOpen);
+// refs.closeBtn.addEventListener("click", onModalClose);
+// refs.backdrop.addEventListener("click", onBackdropClick);
+
+// function onModalOpen() {
+//   window.addEventListener("keydown", onEscKey);
+//   document.body.classList.add("show-modal");
+// }
+
+// function onModalClose() {
+//   window.removeEventListener("keydown", onEscKey);
+//   document.body.classList.remove("show-modal");
+// }
+
+// function onBackdropClick(event) {
+//   if (event.currentTarget === event.target) {
+//     onModalClose();
+//   }
+// }
+
+// function onEscKey(event) {
+//   console.log(event.code);
+//   if (event.code === "Escape") {
+//     onModalClose();
+//   }
+// }
+
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
+
 const refs = {
-  openBtn: document.querySelector('[data-action="open-modal"]'),
-  closeBtn: document.querySelector('[data-action="close-modal"]'),
+  openBtn: document.querySelector("[data-action=open-modal]"),
+  closeBtn: document.querySelector("[data-action=close-modal]"),
   backdrop: document.querySelector(".js-backdrop"),
 };
 
@@ -8,25 +44,16 @@ refs.openBtn.addEventListener("click", onModalOpen);
 refs.closeBtn.addEventListener("click", onModalClose);
 refs.backdrop.addEventListener("click", onBackdropClick);
 
-function onModalOpen() {
-  window.addEventListener("keydown", onEscKey);
+function onModalOpen(event) {
   document.body.classList.add("show-modal");
 }
 
-function onModalClose() {
-  window.removeEventListener("keydown", onEscKey);
+function onModalClose(event) {
   document.body.classList.remove("show-modal");
 }
 
 function onBackdropClick(event) {
   if (event.currentTarget === event.target) {
-    onModalClose();
-  }
-}
-
-function onEscKey(event) {
-  console.log(event.code);
-  if (event.code === "Escape") {
     onModalClose();
   }
 }
