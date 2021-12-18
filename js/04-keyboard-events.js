1ee528ed2c3ad724c6b8406bc869d20dc93fd4b7
woshinivek <woshinivek@yahoo.com>
2021-12-18 18:39:13 +0300

    changes

--- js/04-keyboard-events.js
+++ js/04-keyboard-events.js
@@ -1,20 +1,45 @@
+// const refs = {
+//   btnClear: document.querySelector(".js-clear"),
+//   output: document.querySelector(".js-output"),
+// };
+
+// window.addEventListener("keydown", onKeydown);
+// refs.btnClear.addEventListener("click", onClearOutput);
+
+// function onKeydown(event) {
+//   refs.output.textContent += event.key;
+
+//   console.log("event.code: ", event.code);
+//   console.log("event.key: ", event.key);
+// }
+
+// function onClearOutput(event) {
+//   refs.output.textContent = "";
+
+//   //   location.reload();
+// }
+
 const refs = {
-  btnClear: document.querySelector(".js-clear"),
+  clearBtn: document.querySelector(".js-clear"),
   output: document.querySelector(".js-output"),
 };
 
-window.addEventListener("keydown", onKeydown);
-refs.btnClear.addEventListener("click", onClearOutput);
+window.addEventListener("keypress", onKeyPress);
+// window.addEventListener("keydown", onKeyDown);
 
-function onKeydown(event) {
+refs.clearBtn.addEventListener("click", onClearOutput);
+
+function onKeyPress(event) {
+  console.log(event);
   refs.output.textContent += event.key;
-
-  console.log("event.code: ", event.code);
-  console.log("event.key: ", event.key);
 }
+
+// function onKeyDown(event) {
+//   console.log(event);
+//   refs.output.textContent += event.key;
+// }
 
 function onClearOutput(event) {
   refs.output.textContent = "";
-
-  //   location.reload();
+  location.reload();
 }
