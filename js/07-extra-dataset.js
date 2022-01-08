3ef21fbc0e068bb4529c053601e9ed8fe8bf8b57
woshinivek <woshinivek@yahoo.com>
2022-01-08 17:24:07 +0300

    changes

--- js/07-extra-dataset.js
+++ js/07-extra-dataset.js
@@ -1,9 +1,10 @@
-const colorPrewievRef = document.querySelector("#colors");
+const colorPrewievRef = document.querySelector(".js-color-preview");
 
 const buttons = document.querySelectorAll("button[data-color]");
 
 buttons.forEach((button) => button.addEventListener("click", onChooseBtnClick));
 
 function onChooseBtnClick(evt) {
-  console.log(evt);
+  const element = evt.target;
+  colorPrewievRef.style.backgroundColor = element.dataset.color;
 }
