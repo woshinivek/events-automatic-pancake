cdc10c1261eddce4fcba04b28e662e81f3d5e793
woshinivek <woshinivek@yahoo.com>
2022-01-12 20:26:28 +0300

    changes

---
+++ js/09-01-practice-ashot.js
@@ -0,0 +1,22 @@
+/* 
+1. С использованием значений массива создать 
+маркированный список.
+
+ Создать кнопки 'добавить' и 'удалить', 
+ которые будут 
+ менять состав списка.
+
+ Для оптимизации списка нужно добавить кастомный 
+ атрибут id. 
+
+ В списке четное значение должно красится красным 
+ а не четные в синим цветом.
+*/
+
+const array = ["Toyota", "Bmw", " Aston Martin"];
+
+const constainerRef = document.querySelector(".container");
+const ulRef = document.createElement("ul");
+ulRef.id = "carsContainer";
+
+constainerRef.append(ulRef);
