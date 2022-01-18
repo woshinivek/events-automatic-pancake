980bea7f32e4b0984c206a0c6c3b20eec8a7f470
woshinivek <woshinivek@yahoo.com>
2022-01-18 19:47:19 +0300

    changes

--- js/11-03-practice-ashot.js
+++ js/11-03-practice-ashot.js
@@ -1,6 +1,8 @@
 /* 
 3. Создать небольшую игру :)
-  - Изначально на экране пользователя будет отображаться какая 
+// const rand = Math.floor(Math.random() * (9 - 2 + 1) + 2); 
+
+- Изначально на экране пользователя будет отображаться какая 
     нибудь форма (круг, квадрат, прямоугольник)
   - при нажатии на нее в рандомном порядке форма должна 
   менятся на другую
@@ -10,3 +12,21 @@
     используя цвета из заранее заготовленного массива
   
 */
+
+const forms = [
+  "width: 100px; height: 100px; border-width: 1px; border-color: #000000",
+  "width: 100px; height: 100px; border-radius: 50%; border-width: 1px; border-color: #000000",
+  "width: 150px; height: 100px; border-width: 1px; border-color: #000000",
+];
+
+const colors = ["red", "blue", "green"];
+
+const randomizer = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+const newForm = document.createElement("div");
+// newForm.style.cssText = forms[0];
+// newForm.style.backgroundColor = `${colors[2]}`;
+
+document.body.firstElementChild.append(newForm);
