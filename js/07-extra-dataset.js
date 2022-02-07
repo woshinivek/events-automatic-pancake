5ff3b5e30fb03f54fbb5b34ef1f358ea6fb32fff
woshinivek <woshinivek@yahoo.com>
2022-02-07 16:38:11 +0300

    changes

--- js/07-extra-dataset.js
+++ js/07-extra-dataset.js
@@ -9,36 +9,80 @@
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 
-const colorPrewievRef = document.querySelector(".js-color-preview");
-const colorButtons = document.querySelectorAll("button[data-color]");
-const actionButtons = document.querySelectorAll("button[data-action]");
-const outputRef = document.querySelector(".js-output");
+// const colorPrewievRef = document.querySelector(".js-color-preview");
+// const colorButtons = document.querySelectorAll("button[data-color]");
+// const actionButtons = document.querySelectorAll("button[data-action]");
+// const outputRef = document.querySelector(".js-output");
 
-colorButtons.forEach((button) =>
-  button.addEventListener("click", onChooseColorBtn)
-);
+// colorButtons.forEach((button) =>
+//   button.addEventListener("click", onChooseColorBtn)
+// );
 
-actionButtons.forEach((button) =>
-  button.addEventListener("click", onActionBtnClick)
-);
+// actionButtons.forEach((button) =>
+//   button.addEventListener("click", onActionBtnClick)
+// );
 
-function onChooseColorBtn(evt) {
-  const element = evt.currentTarget;
-  colorPrewievRef.style.backgroundColor = element.dataset.color;
+// function onChooseColorBtn(evt) {
+//   const element = evt.currentTarget;
+//   colorPrewievRef.style.backgroundColor = element.dataset.color;
+// }
+
+// function onActionBtnClick(evt) {
+//   switch (evt.target.dataset.action) {
+//     case "approve":
+//       outputRef.textContent = `Заказ подтвержден`;
+//       break;
+
+//     case "edit":
+//       outputRef.textContent = `Заказ редактируется`;
+//       break;
+
+//     case "delete":
+//       outputRef.textContent = `Заказ удален`;
+//       break;
+
+//     default:
+//       console.log("Ting bu dong");
+//   }
+// }
+
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+
+const refs = {
+  colorsPrev: document.querySelector(".js-color-preview"),
+  colorButtons: document.querySelectorAll("button[data-color]"),
+  actionButtons: document.querySelectorAll("button[data-action]"),
+  output: document.querySelector(".js-output"),
+};
+
+refs.colorButtons.forEach((button) => {
+  button.addEventListener("click", onColorBtnClick);
+});
+
+refs.actionButtons.forEach((button) => {
+  button.addEventListener("click", onActionBtnClick);
+});
+
+function onColorBtnClick(evt) {
+  refs.colorsPrev.style.backgroundColor = evt.currentTarget.dataset.color;
+  //   console.log(evt.currentTarget.dataset.color);
 }
 
 function onActionBtnClick(evt) {
-  switch (evt.target.dataset.action) {
+  refs.output.textContent = evt.currentTarget.dataset.action;
+
+  switch (evt.currentTarget.dataset.action) {
     case "approve":
-      outputRef.textContent = `Заказ подтвержден`;
+      refs.output.textContent = "Заказ подтвержден";
       break;
 
     case "edit":
-      outputRef.textContent = `Заказ редактируется`;
+      refs.output.textContent = "Заказ Редактируется";
       break;
 
     case "delete":
-      outputRef.textContent = `Заказ удален`;
+      refs.output.textContent = "Заказ удален";
       break;
 
     default:
