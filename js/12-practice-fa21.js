aba5d04ce30d92f25a4dd1d03a0cba1a6ea99891
woshinivek <woshinivek@yahoo.com>
2022-02-22 21:05:35 +0300

    changes

--- js/12-practice-fa21.js
+++ js/12-practice-fa21.js
@@ -22,22 +22,33 @@ class ToDoList {
   }
 
   add(toDo) {
-    this.list = [...this.list, newToDo];
+    this.list = [...this.list, toDo];
   }
 
   remove(id) {
     this.list = [...this.list].filter((toDo) => toDo.id !== id);
   }
-}
 
-class ToDo {
-  constructor({ title, description } = {}) {
-    this.id = Date.now();
-    this.title = title;
-    this.description = description;
+  get allTasks() {
+    return this.list;
   }
 }
 
-const first = new ToDo({ title: "Hi", description: "Hello" });
+class ToDo {
+  constructor({ title } = {}) {
+    this.id = Date.now();
+    this.title = title;
+  }
+}
 
-console.log(first);
+const toDoList = new ToDoList();
+
+const firstTask = new ToDo({ title: "Hello tupangi" });
+const secondTask = new ToDo({ title: "putin huilo" });
+
+toDoList.add(firstTask);
+toDoList.add(secondTask);
+
+// console.log(toDoList.allTasks);
+
+// console.log(toDoList);
